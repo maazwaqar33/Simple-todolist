74e4f8da3da68d8cc254e4adea8094127777fb5c
Maaz_ahmed <maazwaqar076@gmail.com>
2024-05-09 16:47:41 +0500

    created npx command

--- index.ts
+++ index.ts
@@ -2,90 +2,151 @@
 
 import inquirer from 'inquirer';
 import chalk from 'chalk';
-  
-// welcome message with chalkANimation
-async function welcomeMessage() {
-    console.log(chalk.bold.hex('#DEADED').bold('Develop By Maaz Ahmed'));
+
+//TodoItem Interface
+interface todoItem{
+    text: String;
+    done : boolean;
 }
 
-await welcomeMessage()
+//Todo List Array
+const todolist : todoItem[] = [];
 
+//Display Todo List Function:
 
-async function userAuth() {
-   
-    let randomId = Math.floor(Math.random() * 4135)
-    console.log(`Your Id is ${randomId}`)
-    let randomPin = Math.floor(Math.random() * 6028)
-    console.log(`Your pin is ${randomPin}`)
-    let isAuth = false;
-    while (!isAuth) {
-       const answers = await inquirer 
-    .prompt([
+function displayTodoList() {
+    console.log(chalk.bold("\n--- Todo List ---"));
+    todolist.forEach((item, index) =>{
+        const status = item.done ? chalk.red('✘') : chalk.green('✔') ;
+        console.log(`${index + 1}. [${status}] ${item.text}`)
+    });
+    console.log("------------------\n");
+}
+
+//Add Todo Function
+function addTodo () {
+    inquirer.prompt([
         {
             type: 'input',
-            name : 'userId',
-            message: 'please enter you id',
-        },
-        {
-            type: 'number',
-            name : 'userPin',
-            message: 'please enter you pin'
-        },
-    ]);
-    const parsedUserID = parseInt(answers.userId)
-    const parsedUserPin = parseInt(answers.userPin)
-
-    if 
-    (parsedUserID === randomId  && parsedUserPin === randomPin)
-    {
-        console.log(chalk.green("Authentication successful! Welcome to the ATM."));
-        isAuth = true;
-    }
-    else{
-        console.log(chalk.red('Invalid user ID or PIN. Please try again.'))
-    }
-    }
-    
-    
+            name: 'text',
+            message: chalk.bold('Enter your todo:')
+        }
+    ]).then((answers) =>{
+        const text = answers.text.trim();
+        if (text !== ''){
+            todolist.push({text: text, done:false});
+            console.log(chalk.green(`"${text}" added to the todo list.`));
+        }else{
+            console.log(chalk.red("Please enter a valid todo item."));
+        }
+        displayTodoList();
+        askAction();
+    })
 }
 
 
-
-async function atmFun() {
-   await userAuth();
-    let replys = await inquirer .prompt([
+//Edit Todo Function
+function editTodo () {
+    
+    inquirer.prompt([
         {
-           type: 'list',
-            name : 'accType',
-            choices:["Saving","current"],
-           message: 'Select your account type'
-       },
-       {
-           type: 'list',
-           name : 'transactionType',
-           choices:["FastCash","withdrawal"],
-           message: 'Select your transaction type',
-           when: replys => replys.accType,
-       
-       },
-       {
-           type: 'list',
-           name : 'amount',
-           choices:["1000","5000", "10000"],
-           message: 'Select your amount',
-           when : replys => replys.transactionType === "FastCash",
-       },
-       {
-           type: 'number',
-           name : 'amount',
-           message: 'Select your amount',
-           when : replys => replys.transactionType === "withdrawal",
-       },
-   ]);
-   if (replys.transactionType === "withdrawal" || "FastCash") {
-       console.log(`You have withdrawn ${replys.amount} from your account`)
-   }
+            type: 'input',
+            name: 'index',
+            message:chalk.bold('Enter index of todo you want to edit:'),
+            validate : (value) =>{
+                const parsedIndex = parseInt(value);
+                if (isNaN(parsedIndex) || parsedIndex < 1 || parsedIndex > todolist.length){
+                    return 'please enter a vlide index'
+                }else{
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'text',
+            message:'Enter new text of you Todo'
+        }
+    ]).then((answers) =>{
+        const index = parseInt(answers.index) -1;
+        const text = answers.text.trim();
 
+        if (text !== ''){
+            todolist[index].text =text;
+            console.log(chalk.yellow(`Todo item ${index + 1} updated.`));
+        }else{
+            console.log(chalk.red("Please enter a valid todo item."));
+           
+        }
+        displayTodoList();
+        askAction();
+    });
 }
 
-atmFun();
+
+//Delete Todo Function
+function deleteTodo () {
+    
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'index',
+            message: chalk.bold('Enter index of todo you want to Delete:'),
+            validate : (value) =>{
+                const parsedIndex = parseInt(value);
+                if (isNaN(parsedIndex) || parsedIndex < 1 || parsedIndex > todolist.length){
+                    return 'please enter a vlide index'
+                }else{
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'text',
+            message:'Enter new text of you Todo'
+        }
+    ]).then((answers) =>{
+        const index = parseInt(answers.index) -1;
+        
+        const deletedItem = todolist.splice(index, 1);
+        
+        console.log(chalk.red(`"${deletedItem[0].text}" deleted from the todo list.`));
+        
+        displayTodoList();
+        askAction();
+    });
+}
+
+
+function askAction (){
+    inquirer .prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message : chalk.bold('What would you like to do?'),
+            choices : ['Add Todo', 'Edit Todo', 'Delete Todo', 'Show Todo List', 'Exit']
+        }
+    ]).then((answers)=>{
+        const action = answers.action.toLowerCase()
+        if(action === 'add todo'){
+            addTodo();
+        }else if (action === 'edit todo'){
+            editTodo();
+        }else if (action === 'delete todo'){
+            deleteTodo();
+        }else if (action === 'show todo list'){
+            displayTodoList();
+            askAction()
+        }else if (action === 'exit'){
+            console.log(chalk.blue("\nThank you for using Todo List CLI!"));
+        }
+    });
+}
+
+function startTodoList(){
+    console.log(chalk.blue("Welcome to Todo List CLI!\n"));
+    askAction();
+}
+
+startTodoList();
